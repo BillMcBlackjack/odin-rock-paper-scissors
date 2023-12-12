f859d6650b017b05dd66e8c31a1fa8f8a86122af
BillMcBlackjack <fabrilongo407@gmail.com>
2023-12-12 19:41:56 +0100

    add a function to play the game in a "best-of-five" mode

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+  const playerSelectionCap = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
   if (playerSelectionCap === computerSelection) {
     return alert("It's a tie!");
   }
@@ -22,16 +23,34 @@ function playRound(playerSelection, computerSelection) {
       (playerSelectionCap === "Paper" && computerSelection === "Rock") ||
       (playerSelectionCap === "Scissors" && computerSelection === "Paper")
     ) {
+        playerScore ++;
         return alert("You win! " + playerSelectionCap + " beats " + computerSelection);
     }
   else {
+    computerScore ++;
     return alert("You lose! " + computerSelection + " beats " + playerSelectionCap);
   }
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("Rock Paper or Scissors?");
-const playerSelectionCap = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+function game() {
+  while(playerScore <= 3 && computerScore <= 3) {
 
-alert("Computer picked " + getComputerChoice());
-playRound(playerSelectionCap, getComputerChoice());
+    if (playerScore === 3) {
+      return alert("Game over! You win!");
+    }
+    else if (computerScore === 3) {
+      return alert ("Game over! You lose!");
+    }
+
+    const playerSelection = prompt("Rock Paper or Scissors?");
+    const computerSelection = getComputerChoice();
+    alert("Computer picked " + computerSelection);
+
+    playRound(playerSelection, computerSelection);
+
+  }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+game();
