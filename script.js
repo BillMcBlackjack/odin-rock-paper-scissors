dccc060a6f51920cf0e86165633a155b00b9acfe
BillMcBlackjack <fabrilongo407@gmail.com>
2023-12-12 19:15:13 +0100

    add a function for the first round of rock-paper-scissors

--- script.js
+++ script.js
@@ -2,14 +2,36 @@ function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * 3) + 1;
   switch (randomNum) {
     case 1:
-      return "rock";
+      return "Rock";
     case 2:
-      return "paper";
+      return "Paper";
     case 3:
-      return "scissors";
+      return "Scissors";
     default:
       // this one handles unexpected cases, but it shouldn't happen in this function
-      return "rock";
+      return "Rock";
   }
 }
 
+function playRound(playerSelection, computerSelection) {
+  if (playerSelectionCap === computerSelection) {
+    return alert("It's a tie!");
+  }
+  else if (
+      (playerSelectionCap === "Rock" && computerSelection === "Scissors") ||
+      (playerSelectionCap === "Paper" && computerSelection === "Rock") ||
+      (playerSelectionCap === "Scissors" && computerSelection === "Paper")
+    ) {
+        return alert("You win! " + playerSelectionCap + " beats " + computerSelection);
+    }
+  else {
+    return alert("You lose! " + computerSelection + " beats " + playerSelectionCap);
+  }
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Rock Paper or Scissors?");
+const playerSelectionCap = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+alert("Computer picked " + getComputerChoice());
+playRound(playerSelectionCap, getComputerChoice());
